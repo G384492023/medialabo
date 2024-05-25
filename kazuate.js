3de27c58c2a37e98df5107a48b85de0bfde87890
G384492023 <238499@st.takushoku-uac.jp>
2024-05-25 12:33:52 +0900

    kadai

--- kazuate.js
+++ kazuate.js
@@ -8,15 +8,14 @@ let kaisu = 0;
 // 予想を4回実行する
 // 将来以下の hantei(); の4回の呼び出しを全て削除する
 // 代わりにここでは，ボタンを押したら hantei() を呼び出すイベント処理をする
-hantei();
-hantei();
-hantei();
-hantei();
+let a = document.querySelector('#print');
+a.addEventListener('click', hantei);
 
 // ボタンを押した後の処理をする関数 hantei() の定義
 function hantei() {
   // 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
-  let yoso = 4;
+  let yoso = document.querySelector('input[name = "seisu"]');
+  let seisu = yoso.valve;
   
   kaisu = kaisu + 1;
 
